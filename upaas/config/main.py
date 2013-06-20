f8ad5cd4678d3932038af76a2436c1d6fa6e2d55
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-06-20 23:18:42 +0200

    main config loader

--- upaas/config/main.py
+++ upaas/config/main.py
@@ -5,7 +5,13 @@
 """
 
 
+import os
+import logging
+
 from upaas.config import base
+
+
+log = logging.getLogger()
 
 
 class UPaaSConfig(base.Config):
@@ -19,3 +25,26 @@ class UPaaSConfig(base.Config):
             "database": base.StringEntry(default="upaas"),
         },
     }
+
+
+def load_main_config():
+    """
+    Try to load main config, return None in case of errors.
+    """
+
+    upaas_config = None
+
+    for path in ['upaas.yml', '/etc/upaas/upaas.yml', '/etc/upaas.yml']:
+        if os.path.isfile(path):
+            try:
+                upaas_config = UPaaSConfig.from_file(path)
+            except base.ConfigurationError:
+                log.error("Invalid config file at %s" % path)
+                return None
+            else:
+                break
+
+    if not upaas_config:
+        log.error("No config file found")
+
+    return upaas_config
