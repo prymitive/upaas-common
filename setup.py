c61af1c88eeff90a0a210dca7c68fc3805cc897d
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-07 23:58:10 +0100

    bump for ppa

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 
 setup(
     name='upaas-common',
-    version='0.2-dev1',
+    version='0.2-dev2',
     license='GPLv3',
     description='uPaaS common classes',
     author='Łukasz Mierzwa',
