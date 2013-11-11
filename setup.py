ecd5ae9c2a8e7544d248d626094675cebfef2e2f
Łukasz Mierzwa <l.mierzwa@gmail.com>
2013-11-11 15:03:18 +0100

    add debian scripts generated using stdeb [ci skip]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='upaas-common',
-    version='0.2-dev7',
+    version='0.2-dev',
     license='GPLv3',
     description='uPaaS common classes',
     author='Łukasz Mierzwa',
