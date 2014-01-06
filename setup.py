c5c52655a5dbb5669bf6249969ac5cc62b66b9a4
Łukasz Mierzwa <l.mierzwa@gmail.com>
2014-01-06 15:19:29 +0100

    0.2.1 release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 setup(
     name='upaas-common',
-    version='0.3-dev',
+    version='0.2.1',
     license='GPLv3',
     description='uPaaS common classes',
     author='Łukasz Mierzwa',
