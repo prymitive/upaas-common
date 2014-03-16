9153e42d3b1925241efeebeb242ed3ed117f70a3
Łukasz Mierzwa <l.mierzwa@gmail.com>
2014-03-16 11:17:27 +0100

    dry test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -7,10 +7,14 @@
 
 from __future__ import unicode_literals
 
+import pytest
+
 from upaas.config.metadata import MetadataConfig
 
 
-MetadataDetect = '''
+@pytest.fixture(scope="module")
+def metadata_detect(request):
+    return MetadataConfig.from_string('''
 interpreter:
   type: ruby
   versions:
@@ -19,10 +23,12 @@ interpreter:
 repository:
   clone: echo "Cloned"
   update: echo "Updated"
-'''
+''')
 
 
-MetadataManual = '''
+@pytest.fixture(scope="module")
+def metadata_manual(request):
+    return MetadataConfig.from_string('''
 interpreter:
   type: ruby
   versions:
@@ -37,56 +43,48 @@ repository:
     date: date cmd
     description: description cmd
     changelog: changelog cmd
-'''
+''')
 
 
-def test_revision_detect():
-    meta = MetadataConfig.from_string(MetadataDetect)
-    assert meta.repository.revision.id() == 'git log -1 --format=%H'
+def test_revision_detect(metadata_detect):
+    assert metadata_detect.repository.revision.id() == 'git log -1 --format=%H'
 
 
-def test_revision_command():
-    meta = MetadataConfig.from_string(MetadataManual)
-    assert meta.repository.revision.id == 'revision id cmd'
+def test_revision_command(metadata_manual):
+    assert metadata_manual.repository.revision.id == 'revision id cmd'
 
 
-def test_author_detect():
-    meta = MetadataConfig.from_string(MetadataDetect)
-    assert meta.repository.revision.author() == \
+def test_author_detect(metadata_detect):
+    assert metadata_detect.repository.revision.author() == \
         "git log -1 --pretty='%aN <%aE>'"
 
 
-def test_author_command():
-    meta = MetadataConfig.from_string(MetadataManual)
-    assert meta.repository.revision.author == 'author cmd'
+def test_author_command(metadata_manual):
+    assert metadata_manual.repository.revision.author == 'author cmd'
 
 
-def test_date_detect():
-    meta = MetadataConfig.from_string(MetadataDetect)
-    assert meta.repository.revision.date() == "git log -1 --pretty='%at'"
+def test_date_detect(metadata_detect):
+    assert metadata_detect.repository.revision.date() == \
+        "git log -1 --pretty='%at'"
 
 
-def test_date_command():
-    meta = MetadataConfig.from_string(MetadataManual)
-    assert meta.repository.revision.date == 'date cmd'
+def test_date_command(metadata_manual):
+    assert metadata_manual.repository.revision.date == 'date cmd'
 
 
-def test_description_detect():
-    meta = MetadataConfig.from_string(MetadataDetect)
-    assert meta.repository.revision.description() == "git log -1 --pretty='%B'"
+def test_description_detect(metadata_detect):
+    assert metadata_detect.repository.revision.description() == \
+        "git log -1 --pretty='%B'"
 
 
-def test_description_command():
-    meta = MetadataConfig.from_string(MetadataManual)
-    assert meta.repository.revision.description == 'description cmd'
+def test_description_command(metadata_manual):
+    assert metadata_manual.repository.revision.description == 'description cmd'
 
 
-def test_changelog_detect():
-    meta = MetadataConfig.from_string(MetadataDetect)
-    assert meta.repository.revision.changelog() == \
+def test_changelog_detect(metadata_detect):
+    assert metadata_detect.repository.revision.changelog() == \
         'git log --no-merges --format=medium %old%..%new%'
 
 
-def test_changelog_command():
-    meta = MetadataConfig.from_string(MetadataManual)
-    assert meta.repository.revision.changelog == 'changelog cmd'
+def test_changelog_command(metadata_manual):
+    assert metadata_manual.repository.revision.changelog == 'changelog cmd'
